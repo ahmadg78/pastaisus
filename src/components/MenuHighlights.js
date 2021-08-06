8a00cb0e31dff63a4fd2d726eb302fa00d507fe6
ahmad goldsboro <ahmadgoldsboro@yahoo.com>
2021-08-05 19:31:24 -0500

    6.gallery

--- src/components/MenuHighlights.js
+++ src/components/MenuHighlights.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import home1 from '../img/home1.jpg';
+import home2 from '../img/home2.jpg';
 import home3 from '../img/home3.jpg';
 import home4 from '../img/home4.jpg';
 import home5 from '../img/home5.jpg';
-import home6 from '../img/home5.jpg';
+import home6 from '../img/home6.jpg';
 import home7 from '../img/home7.jpg'
 import home8 from '../img/home8.jpg';
 import home9 from '../img/home9.jpg';
@@ -14,100 +16,100 @@ import { About, Description, Image } from './styles';
 
 const MenuHighlights = () => {
     return (
-        <div>
-            <h2>Menu Highlights</h2>
-        <Gallery>
-                      <Image className='item1'>
-                            <img  src={home3} alt='hi'></img>
-                            <button>Family style meals</button>
-                        </Image>
-                        <Image  className='item2'>
-                            <img src={home4} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-                        <Image className='item3'>
-                            <img  src={home7} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-                         <Image className='item4' >
-                            <img src={home6} alt='hi'></img>
-                            <button>Create your own pasta</button>
-                        </Image>
-                        <Image className='item5'>
-                            <img src={home8} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-                        <Image className='item6'>
-                            <img  src={home9} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-                        <Image className='item7'>
-                            <img  src={home10} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-                        <Image className='item8'>
-                            <img   src={home11} alt='hi'></img>
-                            <button>Appetizers</button>
-                        </Image>
-        </Gallery>
+        <div className='container'>
+            <h2>Season specials</h2>
+            <Gallery>
+            <Image className='gallery_item1'>
+                <img  src={home1} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item2'>
+                <img  src={home2} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item3'>
+                <img  src={home3} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item4'>
+                <img  src={home4} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item5'>
+                <img  src={home5} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item6'>
+                <img  src={home6} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item7'>
+                <img  src={home7} alt='hi'></img>
+            </Image>
+            <Image className='gallery_item8'>
+                <img  src={home8} alt='hi'></img>
+            </Image>
+            </Gallery>
         </div>
     );
 };
 
 const Gallery = styled.div`
+    /* .item1{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    } */
     display: grid;
-    grid-template-columns: repeat(8,1fr);
-    grid-template-rows: repeat(8, 5vw);
-    grid-gap: 20px;
-    .item1{
-        grid-column-start: 1;
-        grid-column-end: 3;
-        grid-row-start: 1;
-        grid-row-end:3;
+    grid-template-columns: 200px 200px 200px 200px  ;
+    grid-template-rows: 300px 300px 300px 300px 300px;
+    grid-gap: 10px;
+     .gallery_item1{
+         grid-column-start: 1;
+         grid-column-end: 3;
+         grid-row-start: 1;
+         grid-row-end: 2;
+       
     }
-    .item2{
+    .gallery_item2{
+       grid-column-start: 3;
+       grid-column-end: 6;
+       grid-row-start: 1;
+       grid-row-end: 3;
+    }
+    .gallery_item3{
+       grid-column-start: 1;
+       grid-column-end: 3;
+       grid-row-start: 2;
+       grid-row-end: 4;
+    }
+    .gallery_item4{
         grid-column-start: 3;
-        grid-column-end: 6;
-        grid-row-start: 1;
-        grid-row-end:3;
-    }
-    .item3{
-        grid-column-start: 5;
-        grid-column-end: 9;
-        grid-row-start: 1;
-        grid-row-end:6;
-    }
-    .item4{
-        grid-column-start: 1;
         grid-column-end: 5;
         grid-row-start: 3;
-        grid-row-end:6;
+        grid-column-end: 5;
+     
     }
-    .item5{
-        grid-column-start: 1;
-        grid-column-end: 4;
-        grid-row-start: 6;
-        grid-row-end: 9;
-    }  
-    .item6{
+    .gallery_item5{
         grid-column-start: 5;
-        grid-column-end: 9;
-        grid-row-start: 6;
-        grid-row-end: 9;
+        grid-column-end: 6;
+        grid-row-start: 3;
+        grid-row-end: 5;
     }  
-    .item7{
-        grid-column-start: 1;
-        grid-column-end: 4;
-        grid-row-start: 6;
-        grid-row-end: 9;
-    } 
-    .item8{
-        grid-column-start: 1;
-        grid-column-end: 4;
-        grid-row-start: 6;
-        grid-row-end: 9;
+    .gallery_item6{
+      grid-column-start: 1;
+      grid-column-end: 5;
+      grid-row-start: 4;
+      grid-row-end: 5;
     }  
+     .gallery_item7{
+         grid-column-start: 1;
+         grid-column-end: 3;
+         grid-row-start: 5;
+         grid-row-end: 6;
    
+    } 
+    .gallery_item8{
+        grid-column-start: 3;
+        grid-column-end: 6;
+        grid-row-start: 5;
+        grid-row-end: 6;
+        
+     } 
 `;
 
 export default MenuHighlights;
